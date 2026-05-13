00d00f7f7fab3dd002b776288b2709aeef991ba7
AlvinFandiSyahrizal <alvinfandi43@gmail.com>
2026-05-13 15:40:09 +0700

    feat: database migrations and models with relationships

--- app/Models/CltLayer.php
+++ app/Models/CltLayer.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class CltLayer extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['layup_id', 'layer_order', 'thickness', 'width', 'angle'];
+
+    public function layup()
+    {
+        return $this->belongsTo(CltLayup::class, 'layup_id');
+    }
 }
--- app/Models/CltLayup.php
+++ app/Models/CltLayup.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class CltLayup extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['supplier_id', 'name'];
+
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class);
+    }
+
+    public function layers()
+    {
+        return $this->hasMany(CltLayer::class, 'layup_id');
+    }
 }
